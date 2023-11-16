6f76d7c2fb90908271ac8209b29949d788b0de87
nhat8002nguyen <nv.nhat8002@gmail.com>
2023-11-16 20:13:37 +0700

    update

--- scrapy_projects/upwork/business_info_projects/masothue_dot_com/masothue_dot_com/settings.py
+++ scrapy_projects/upwork/business_info_projects/masothue_dot_com/masothue_dot_com/settings.py
@@ -30,7 +30,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.1 
+DOWNLOAD_DELAY = 0.2 
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 8
 # CONCURRENT_REQUESTS_PER_IP = 8
@@ -75,9 +75,9 @@ DOWNLOADER_MIDDLEWARES = {
 # Setting for zyte smart proxy manager
 # ZYTE_SMARTPROXY_ENABLED = True
 # ZYTE_SMARTPROXY_APIKEY = os.environ["ZYTE_SMART_PROXY_API_KEY"]
+# AUTOTHROTTLE_ENABLED = False
 CONCURRENT_REQUESTS = 32
 CONCURRENT_REQUESTS_PER_DOMAIN = 32
-# AUTOTHROTTLE_ENABLED = False
 DOWNLOAD_TIMEOUT = 3000
 
 # Enable or disable extensions
