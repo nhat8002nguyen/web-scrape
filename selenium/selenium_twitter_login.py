de9f0c83c52a8cee038750d88d55b4d2d1529f61
nhat8002nguyen <nv.nhat8002@gmail.com>
2023-11-23 13:17:38 +0700

    update twitter login, update masothue crawler state

--- selenium/selenium_twitter_login.py
+++ selenium/selenium_twitter_login.py
@@ -5,32 +5,41 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.webdriver import WebDriver
 
 import pandas as pd
 import time
 import os
 import dotenv
 
-dotenv.load_dotenv()
+def main():
+	dotenv.load_dotenv()
+	website = 'https://twitter.com/'
 
-website = 'https://twitter.com/'
+	path = '.\chromedriver.exe'
+	for root, dirs, files in os.walk(os.getcwd()):
+		for name in files:
+			if 'chromedriver' in name:
+				path = os.path.join(root, name)			
 
-path = '/usr/bin/chromedriver'
+	os_username = os.environ["USERNAME"]
+	user_data_dir = f"C:\\Users\\{os_username}\\AppData\\Local\\Google\\Chrome\\User Data"
 
+	chrome_options = Options()
+	# chrome_options.add_argument('--headless')
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument('--disable-dev-shm-usage')
+	chrome_options.add_argument('--start-maximized')
+	# chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 
-chrome_options = Options()
-# chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--start-maximized')
-# chrome_options.add_argument('--auto-open-devtools-for-tabs')
+	service = Service(executable_path=path)
+	driver = webdriver.Chrome(options=chrome_options, service=service)
+	driver.get(website)
+	wait = WebDriverWait(driver, 100)
 
-service = Service(executable_path=path)
-driver = webdriver.Chrome(options=chrome_options, service=service)
-driver.get(website)
-wait = WebDriverWait(driver, 5)
+	login_to_twitter(driver, wait)
 
-def login_to_twitter():
+def login_to_twitter(driver: WebDriver, wait: WebDriverWait):
 	# move to the login form
 	login_btn = driver.find_element(by=By.XPATH, value='//a[@href="/login"]')
 	login_btn.click()
@@ -55,4 +64,5 @@ def login_to_twitter():
 
 	time.sleep(120)
 
-login_to_twitter()
+if __name__ == "__main__":
+	main()
