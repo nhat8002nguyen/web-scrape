fb74d132fda0cea965f0c64e269f7ccaaeeca295
nhat8002nguyen <nv.nhat8002@gmail.com>
2023-12-16 22:49:56 +0700

    export file for each batch

--- selenium/blinkist/main-v1.py
+++ selenium/blinkist/main-v1.py
@@ -47,6 +47,7 @@ categories = [
     "https://www.blinkist.com/en/nc/categories/creativity-en/books",
 ]
 
+BATCH_LENGTH = 50
 
 class Book():
     author: str
@@ -62,12 +63,101 @@ class Book():
         self.key_ideas = key_ideas
 
 
+class ExportFrame():
+    author_list = []
+    book_name_list = []
+    introduction_list = []
+    section0s = []
+    section1s = []
+    section2s = []
+    section3s = []
+    section4s = []
+    section5s = []
+    section6s = []
+    section7s = []
+    section8s = []
+    section9s = []
+    section10s = []
+    section11s = []
+    section12s = []
+    section13s = []
+    section14s = []
+    section15s = []
+    section16s = []
+    section17s = []
+    section18s = []
+    section19s = []
+
+    def exportXLSX(self, cat_num: int, start_index: int, end_index: int):
+        self.df = pd.DataFrame({
+            "Author": self.author_list,
+            "Book Name": self.book_name_list,
+            "Introduction": self.introduction_list,
+            "Section 1": self.section0s,
+            "Section 2": self.section1s,
+            "Section 3": self.section2s,
+            "Section 4": self.section3s,
+            "Section 5": self.section4s,
+            "Section 6": self.section5s,
+            "Section 7": self.section6s,
+            "Section 8": self.section7s,
+            "Section 9": self.section8s,
+            "Section 10": self.section9s,
+            "Section 11": self.section10s,
+            "Section 12": self.section11s,
+            "Section 13": self.section12s,
+            "Section 14": self.section13s,
+            "Section 15": self.section14s,
+            "Section 16": self.section15s,
+            "Section 17": self.section16s,
+            "Section 18": self.section17s,
+            "Section 19": self.section18s,
+            "Section 20": self.section19s,
+        })
+
+        self.df.to_excel(
+            f"{os.environ['ABSOLUTE_PATH']}/blinkist-output-categ-{cat_num+1}-{start_index}-{end_index}.xlsx", index=False)
+
+        self.clear()
+
+    def exportJson(self, cat_num: int, start_index: int, end_index: int):
+        if self.df != None:
+            self.df.to_json(
+                f"{os.environ['ABSOLUTE_PATH']}/blinkist-output-categ-{cat_num+1}-{start_index}-{end_index}.json", index=False)
+        
+        self.clear()
+
+    def clear(self):
+        self.author_list = []
+        self.book_name_list = []
+        self.introduction_list = []
+        self.section0s = []
+        self.section1s = []
+        self.section2s = []
+        self.section3s = []
+        self.section4s = []
+        self.section5s = []
+        self.section6s = []
+        self.section7s = []
+        self.section8s = []
+        self.section9s = []
+        self.section10s = []
+        self.section11s = []
+        self.section12s = []
+        self.section13s = []
+        self.section14s = []
+        self.section15s = []
+        self.section16s = []
+        self.section17s = []
+        self.section18s = []
+        self.section19s = []
+
+
 def main():
     print("Started the program!")
     dotenv.load_dotenv()
 
-    driver = Driver(uc=True, no_sandbox=True, headless=True,
-                    proxy="webshareio005844-rotate:webshareio005844@p.webshare.io:80")
+    driver = Driver(uc=True, no_sandbox=True, headless=True)
     driver.get('https://www.blinkist.com/en/nc/login/')
     wait = WebDriverWait(driver, 60)
 
@@ -79,7 +169,7 @@ def main():
         cookie_button.click()
         time.sleep(1)
     except Exception as err:
-        print(err)
+        print("Cookie button not found!")
         pass
 
     try:
@@ -101,12 +191,12 @@ def main():
         )))
         submit_input.click()
     except Exception as err:
-        logAndExit(driver, start_cate, end_cate, err)
+        logAndExit(driver, start_cate, end_cate, "login fail!")
 
     time.sleep(5)
 
     start_cate = 17
-    end_cate = 22 
+    end_cate = 27
     for i in range(start_cate, end_cate):
         start_time = time.perf_counter()
         driver.get(categories[i])
@@ -117,111 +207,90 @@ def main():
                 '//a[@class="letter-book-list__item"]'
             )))
         except Exception as err:
-            logAndExit(driver, start_cate, end_cate, err)
-            
+            logAndExit(driver, start_cate, end_cate,
+                       "Could not found book items!")
+
         book_urls = [item.get_attribute("href") for item in book_items]
 
-        author_list = []
-        book_name_list = []
-        introduction_list = []
-        section0s = []
-        section1s = []
-        section2s = []
-        section3s = []
-        section4s = []
-        section5s = []
-        section6s = []
-        section7s = []
-        section8s = []
-        section9s = []
-        section10s = []
-        section11s = []
-        section12s = []
-        section13s = []
-        section14s = []
-        section15s = []
-        section16s = []
-        section17s = []
-        section18s = []
-        section19s = []
+        exportFrame = ExportFrame()
 
         fail_urls = []
         num_book_urls = len(book_urls)
-        for url in book_urls:
+        batch_start_index = 0
+        for j in range(num_book_urls):
             try:
-                bookData = scrapeDataFromBookUrl(driver, wait, url)
+                bookData = scrapeDataFromBookUrl(driver, wait, book_urls[j])
             except:
-                fail_urls.append(url)
+                fail_urls.append(book_urls[j])
                 continue
 
             # add to data export
-            author_list.append(bookData.author)
-            book_name_list.append(bookData.book_name)
-            introduction_list.append(bookData.introduction)
+            exportFrame.author_list.append(bookData.author)
+            exportFrame.book_name_list.append(bookData.book_name)
+            exportFrame.introduction_list.append(bookData.introduction)
 
             key_ideas = bookData.key_ideas
-            section0s.append((key_ideas[0] if 0 < len(key_ideas) else ""))
-            section1s.append((key_ideas[1] if 1 < len(key_ideas) else ""))
-            section2s.append((key_ideas[2] if 2 < len(key_ideas) else ""))
-            section3s.append((key_ideas[3] if 3 < len(key_ideas) else ""))
-            section4s.append(key_ideas[4] if 4 < len(key_ideas) else "")
-            section5s.append(key_ideas[5] if 5 < len(key_ideas) else "")
-            section6s.append(key_ideas[6] if 6 < len(key_ideas) else "")
-            section7s.append(key_ideas[7] if 7 < len(key_ideas) else "")
-            section8s.append(key_ideas[8] if 8 < len(key_ideas) else "")
-            section9s.append(key_ideas[9] if 9 < len(key_ideas) else "")
-            section10s.append(key_ideas[10] if 10 < len(key_ideas) else "")
-            section11s.append(key_ideas[11] if 11 < len(key_ideas) else "")
-            section12s.append(key_ideas[12] if 12 < len(key_ideas) else "")
-            section13s.append(key_ideas[13] if 13 < len(key_ideas) else "")
-            section14s.append(key_ideas[14] if 14 < len(key_ideas) else "")
-            section15s.append(key_ideas[15] if 15 < len(key_ideas) else "")
-            section16s.append(key_ideas[16] if 16 < len(key_ideas) else "")
-            section17s.append(key_ideas[17] if 17 < len(key_ideas) else "")
-            section18s.append(key_ideas[18] if 18 < len(key_ideas) else "")
-            section19s.append(key_ideas[19] if 19 < len(key_ideas) else "")
+            exportFrame.section0s.append(
+                (key_ideas[0] if 0 < len(key_ideas) else ""))
+            exportFrame.section1s.append(
+                (key_ideas[1] if 1 < len(key_ideas) else ""))
+            exportFrame.section2s.append(
+                (key_ideas[2] if 2 < len(key_ideas) else ""))
+            exportFrame.section3s.append(
+                (key_ideas[3] if 3 < len(key_ideas) else ""))
+            exportFrame.section4s.append(
+                key_ideas[4] if 4 < len(key_ideas) else "")
+            exportFrame.section5s.append(
+                key_ideas[5] if 5 < len(key_ideas) else "")
+            exportFrame.section6s.append(
+                key_ideas[6] if 6 < len(key_ideas) else "")
+            exportFrame.section7s.append(
+                key_ideas[7] if 7 < len(key_ideas) else "")
+            exportFrame.section8s.append(
+                key_ideas[8] if 8 < len(key_ideas) else "")
+            exportFrame.section9s.append(
+                key_ideas[9] if 9 < len(key_ideas) else "")
+            exportFrame.section10s.append(
+                key_ideas[10] if 10 < len(key_ideas) else "")
+            exportFrame.section11s.append(
+                key_ideas[11] if 11 < len(key_ideas) else "")
+            exportFrame.section12s.append(
+                key_ideas[12] if 12 < len(key_ideas) else "")
+            exportFrame.section13s.append(
+                key_ideas[13] if 13 < len(key_ideas) else "")
+            exportFrame.section14s.append(
+                key_ideas[14] if 14 < len(key_ideas) else "")
+            exportFrame.section15s.append(
+                key_ideas[15] if 15 < len(key_ideas) else "")
+            exportFrame.section16s.append(
+                key_ideas[16] if 16 < len(key_ideas) else "")
+            exportFrame.section17s.append(
+                key_ideas[17] if 17 < len(key_ideas) else "")
+            exportFrame.section18s.append(
+                key_ideas[18] if 18 < len(key_ideas) else "")
+            exportFrame.section19s.append(
+                key_ideas[19] if 19 < len(key_ideas) else "")
 
             time.sleep(3)
 
-        fail_df = pd.DataFrame({
-            "urls": fail_urls
-        })
-        fail_df.to_json(
-            f"{os.environ['ABSOLUTE_PATH']}fail-url-categ-{i+1}.json", index=False)
+            if (j+1) % BATCH_LENGTH == 0 or j == num_book_urls-1:
+                fail_df = pd.DataFrame({
+                    "urls": fail_urls
+                })
+                fail_df.to_json(
+                    f"{os.environ['ABSOLUTE_PATH']}/fail-url-categ-{i+1}-{batch_start_index}-{j}.json", index=False)
+                fail_urls = []
 
-        df = pd.DataFrame({
-            "Author": author_list,
-            "Book Name": book_name_list,
-            "Introduction": introduction_list,
-            "Section 1": section0s,
-            "Section 2": section1s,
-            "Section 3": section2s,
-            "Section 4": section3s,
-            "Section 5": section4s,
-            "Section 6": section5s,
-            "Section 7": section6s,
-            "Section 8": section7s,
-            "Section 9": section8s,
-            "Section 10": section9s,
-            "Section 11": section10s,
-            "Section 12": section11s,
-            "Section 13": section12s,
-            "Section 14": section13s,
-            "Section 15": section14s,
-            "Section 16": section15s,
-            "Section 17": section16s,
-            "Section 18": section17s,
-            "Section 19": section18s,
-            "Section 20": section19s,
-        })
+                try:
+                    exportFrame.exportXLSX(i, batch_start_index, j)
+                    print(
+                        f"Successfully saved xlsx data from {batch_start_index} to {j}!")
+                except:
+                    exportFrame.exportJson(i, batch_start_index, j)
+                    logAndExit(driver, start_cate, end_cate,
+                               "Could not export to excel file")
 
-        try:
-            df.to_excel(
-                f"{os.environ['ABSOLUTE_PATH']}/blinkist-output-categ-{i+1}-all.xlsx", index=False)
-        except:
-            df.to_json(
-                f"{os.environ['ABSOLUTE_PATH']}/blinkist-output-categ-{i+1}-all.json", index=False)
-            logAndExit(driver, start_cate, end_cate, err)
+                batch_start_index = j+1
 
         print(
             f"Successfully scraped {num_book_urls} books in the category number {i+1}!")
@@ -231,11 +300,13 @@ def main():
         print(f"Delay {cate_delay} seconds after moving to next category")
         time.sleep(cate_delay)
 
+
 def logAndExit(driver, start_cate, end_cate, err):
     with open(f"log-{start_cate}-{end_cate}.txt", "x") as file:
-        file.write(f"Error: {err}") 
+        file.write(f"Error: {err}")
     driver.close()
     exit()
+
 
 def scrapeDataFromBookUrl(driver: WebDriver, driver_wait: WebDriverWait, url: str) -> Book:
     book = Book("", "", "", list())
@@ -329,10 +400,10 @@ def scrapeDataFromBookUrl(driver: WebDriver, driver_wait: WebDriverWait, url: st
         else:
             book.key_ideas.append(content)
 
-        # print(chapter)
-        # print(title)
-        # print(description)
-        # print("--------------------------------------------------------------")
+        print(chapter)
+        print(title)
+        print(description)
+        print("--------------------------------------------------------------")
 
     return book
 
